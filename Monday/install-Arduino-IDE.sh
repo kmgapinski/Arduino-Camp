393beb6951732d7479c5575cabcce4ba1923e618
Zubacat <trevor.vranicar@gmail.com>
2015-03-14 12:14:07 -0400

    Arduino-IDE update, git-ign upd

--- Monday/install-Arduino-IDE.sh
+++ Monday/install-Arduino-IDE.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "Download Arduino IDE"
-url=http://arduino.cc/download.php?f=/arduino-1.0.6-linux64.tgz
+url=http://arduino.cc/download_handler.php?f=/arduino-1.6.1-linux64.tar.xz
+#Arduino 'Classic' IDE
+#url=http://arduino.cc/download.php?f=/arduino-1.0.6-linux64.tgz
 filename=$(basename "$url")
 wget -O $filename "$url"
-tar -zxvf $filename
+tar -xf $filename
+arduino_dir=$(ls | grep arduino | grep -v tar)
 rm $filename
 
 echo "Download Vilros USK code and Install"
@@ -14,15 +17,15 @@ unzip $filename -d $PWD
 rm $filename
 
 echo "Move Examples into Arduino IDE"
-mv USK\ Guide\ Code/* arduino-1.0.6/examples/
+mv USK\ Guide\ Code/* $arduino_dir/examples/
 rm -r USK\ Guide\ Code/
 
 echo "Try to Create Desktop link"
 cat > ~/Desktop/Arduino.desktop <<_EOF_
 [Desktop Entry]
 Name=Arduino
-Exec=$PWD/arduino-1.0.6/arduino
-Icon=$PWD/arduino-1.0.6/reference/img/logo.png
+Exec=$PWD/$arduino_dir/arduino
+Icon=$PWD/$arduino_dir/lib/arduino_icon.ico
 Terminal=false
 Type=Application
 _EOF_
